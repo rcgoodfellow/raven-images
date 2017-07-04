57d912a9d397ae183fe946c56f41928cefa6d8d2
ry <rgoodfel@isi.edu>
2017-07-04 11:33:11 -0700

    hit mirror.deterlab.net for apt

--- scripts/debian/init.sh
+++ scripts/debian/init.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-set -e
 set -x
 
-sudo sed -i -e 's,^.*:/sbin/getty\s\+.*\s\+tty[2-6],#\0,' /etc/inittab
+sudo apt-get install -y apt-transport-https
+
+cat >/tmp/sources.list << EOF
+deb http://mirror.deterlab.net/mirrors/debian/ stretch main contrib non-free
+deb-src http://mirror.deterlab.net/mirrors/debian/ stretch main
+
+deb http://mirror.deterlab.net/mirrors/debian-security stretch/updates main
+deb-src http://mirror.deterlab.net/mirrors/debian-security stretch/updates main
+EOF
+
+sudo cp /tmp/sources.list /etc/apt/sources.list
